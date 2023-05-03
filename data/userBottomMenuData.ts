c5744c070da178b623e370b4e0d29b33226f044d
unknown <fuzzfuzz20@naver.com>
2023-05-03 11:02:07 +0900

    feat:마이페이지 bottom Section data 추가

---
+++ data/userBottomMenuData.ts
@@ -0,0 +1,44 @@
+export const userBottomMenuData = [
+  {
+    id: 1,
+    name: '출석하기',
+    iconUrl: "/assets/images/icons/jewelry.svg",
+    path: "/attendance"
+  },
+  {
+    id: 2,
+    name: '회원정보 변경',
+    iconUrl: "/assets/images/icons/InformationChange.svg",
+    path: "/user-info"
+  },
+  {
+    id: 3,
+    name: '블럭 이용 내역',
+    iconUrl: "/assets/images/mypage/Block.png",
+    path: "/block-history"
+  },
+  {
+    id: 4,
+    name: 'NFT 보관함',
+    iconUrl: "/assets/images/icons/NFTBox.svg",
+    path: "/nft-box"
+  },
+  {
+    id: 5,
+    name: '작가 등록',
+    iconUrl: "/assets/images/icons/UserPlus.svg",
+    path: "/user-register"
+  },
+  {
+    id: 6,
+    name: '작가 관리',
+    iconUrl: "/assets/images/icons/author.svg",
+    path: "/user-management"
+  },
+  {
+    id: 7,
+    name: '로그아웃',
+    iconUrl: "/assets/images/icons/logout.svg",
+    path: "/logout"
+  }
+]
