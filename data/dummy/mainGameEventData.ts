9df266427150db07cb77d755d71bd34a189166d3
Jason Ahn <beat1103@gmail.com>
2023-05-16 17:07:38 +0900

    serverside props

--- data/dummy/mainGameEventData.ts
+++ data/dummy/mainGameEventData.ts
@@ -8,7 +8,7 @@ export const gameEventData = [
     path: "/game/1",
   },
   {
-    id: 1,
+    id: 2,
     eventTitle: "돌려라 룰렛!!",
     text: "데일리 게임으로 무료 블럭 획득하기",
     imgUrl: "/assets/dummy/02.png",
