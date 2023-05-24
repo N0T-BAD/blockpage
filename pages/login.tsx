55afc220952cf40ca59857a1bc9756a185689bd6
Jason Ahn <beat1103@gmail.com>
2023-05-24 17:04:58 +0900

    login

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,8 +3,12 @@ import LoginSection from "@/components/pages/login/LoginSection"
 import LoginLayout from "@/components/layouts/LoginLayout"
 import { useSession, signIn, getCsrfToken } from "next-auth/react"
 import Image from "next/image"
+import { useEffect, useState } from "react"
 
 const login: NextPageWithLayout = () => {
+
+  const { data } = useSession()
+  
   
   return (
     <LoginSection />
