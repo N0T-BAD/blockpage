6ab3c2c67e8201463206bc7d7b8e722534539fa6
goeun <fuzzfuzz20@naver.com>
2023-06-08 22:45:30 +0900

    feat: 게임 수정중

--- components/games/RouletteGame.tsx
+++ components/games/RouletteGame.tsx
@@ -55,10 +55,10 @@ export default function RouletteGame() {
     },
   ];
 
-  const [spinning, setSpinning] = useState<boolean>(false); 
-  const [prize, setPrize] = useState<number>(Math.floor(Math.random() * prizeData.length)+1); 
+  const [spinning, setSpinning] = useState<boolean>(false);
+  const [prize, setPrize] = useState<number>(Math.floor(Math.random() * prizeData.length) + 1);
   const [prizeNumber, setPrizeNumber] = useState<number>(0);
-  const [getPrize, setGetPrize] = useState<boolean>(false); 
+  const [getPrize, setGetPrize] = useState<boolean>(false);
 
   const handleClick = () => {
     setSpinning(true);
@@ -67,73 +67,73 @@ export default function RouletteGame() {
     setTimeout(() => {
       setSpinning(false);
       setGetPrize(true);
-      setPrize(Math.floor(Math.random() * prizeData.length)+1);
+      setPrize(Math.floor(Math.random() * prizeData.length) + 1);
     }, 4000);
   };
 
   return (
     <div className={style.wrap}>
       <div className={style.contents}>
-          <div className={style.rouletteOuter}>
-            <div className={ spinning ? `${style.roulette} ${style[`on${prize}`]}` : style.roulette }> 
-              {/* 값 영역 */}
-              <div>
-                {prizeData.map((item) => {    // data map
-                  return (
-                    <div
-                      style={{ transform: `rotate(${((item.id - 1) * 45)%360}deg)` }}  // 아이템 배치 각도
-                      className={style.item}
-                      key={item.id}
-                    >
+        <div className={style.rouletteOuter}>
+          <div className={spinning ? `${style.roulette} ${style[`on${prize}`]}` : style.roulette}>
+            {/* 값 영역 */}
+            <div>
+              {prizeData.map((item) => {    // data map
+                return (
+                  <div
+                    style={{ transform: `rotate(${((item.id - 1) * 45) % 360}deg)` }}  // 아이템 배치 각도
+                    className={style.item}
+                    key={item.id}
+                  >
+                    <div>
                       <div>
-                        <div>
-                          <p className={style.brandName}>{item.brandName}</p>
-                          <p className={style.prizeName}>{item.brandName}</p>
-                        </div>
-                        <div className={style.prizeImg}>
-                          <Image
-                            className={style.prizeImgUrl}
-                            src={item.prizeImgUrl}
-                            alt={item.prizeImgUrl}
-                            width={50}
-                            height={50}
-                          />
-                        </div>
+                        <p className={style.brandName}>{item.brandName}</p>
+                        <p className={style.prizeName}>{item.brandName}</p>
+                      </div>
+                      <div className={style.prizeImg}>
+                        <Image
+                          className={style.prizeImgUrl}
+                          src={item.prizeImgUrl}
+                          alt={item.prizeImgUrl}
+                          width={50}
+                          height={50}
+                        />
                       </div>
                     </div>
-                  );
-                })}
-              </div>
-              {/* 선 영역 */}
-              <div>
-                {[...Array(8)].map((n, index) => {
-                  return (
-                    <div
-                      className={style.line}
-                      style={{
-                        transform: `rotate(${(index + 1) * 45 - 22.5}deg)`,  // 선의 각도
-                      }}
-                      key={index}
-                    />
-                  );
-                })}
-              </div>
+                  </div>
+                );
+              })}
             </div>
-            {/* <FiIcon className={style.roulettePin} />   // 상단에 있는 룰렛 핀 아이콘 */}
-            <div className={style.rouletteOuterBtn}>
-              <button
-                className={style.rouletteBtn}
-                onClick={handleClick}
-              >
-                <p>도전</p>
-              </button>
+            {/* 선 영역 */}
+            <div>
+              {[...Array(8)].map((n, index) => {
+                return (
+                  <div
+                    className={style.line}
+                    style={{
+                      transform: `rotate(${(index + 1) * 45 - 22.5}deg)`,  // 선의 각도
+                    }}
+                    key={index}
+                  />
+                );
+              })}
             </div>
           </div>
+          {/* <FiIcon className={style.roulettePin} />   // 상단에 있는 룰렛 핀 아이콘 */}
+          <div className={style.rouletteOuterBtn}>
+            <button
+              className={style.rouletteBtn}
+              onClick={handleClick}
+            >
+              <p>도전</p>
+            </button>
+          </div>
         </div>
-        {
-          getPrize &&
-          <p style={{textAlign:'center', marginTop: '1rem'}}>당첨 번호 : {prizeNumber}</p>
-        }
+      </div>
+      {
+        getPrize &&
+        <p style={{ textAlign: 'center', marginTop: '1rem' }}>당첨 번호 : {prizeNumber}</p>
+      }
     </div>
   )
 }
