770922a94d9670b83e7a96b6edb2c9d604a3076a
goeun <fuzzfuzz20@naver.com>
2023-06-09 01:03:52 +0900

    feat: 게임 퍼블리싱

--- pages/game/roulette.tsx
+++ pages/game/roulette.tsx
@@ -2,6 +2,7 @@ import RouletteGame from '@/components/games/RouletteGame'
 import Layout from '@/components/layouts/layout'
 import React from 'react'
 import { NextPageWithLayout } from '../_app'
+import { Params } from 'next/dist/shared/lib/router/utils/route-matcher'
 
 const Roulette: NextPageWithLayout = () => {
 
@@ -21,5 +22,15 @@ Roulette.getLayout = function getLayout(page: React.ReactElement) {
 }
 
 export default Roulette;
+
+export async function getServerSideProps(context: Params) {
+  const gameId = parseInt(context.gameId);
+
+  return {
+    props: {
+      gameId,
+    },
+  };
+}
 
 Roulette.auth = true
