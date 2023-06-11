0558a1f1a8f760cfcf754265f6943d6157f8f819
swh9122 <swh9122@gmail.com>
2023-06-12 01:21:40 +0900

    feat: 최근검색어 기능 추가

--- components/pages/search/SearchBar.tsx
+++ components/pages/search/SearchBar.tsx
@@ -5,12 +5,17 @@ import { useRouter } from 'next/router';
 import style from '@/components/pages/search/SearchBar.module.css'
 import BackBtn from '@/components/ui/BackBtn';
 
-export default function SearchBar(props: { handleSearch: () => void, handleOnKeyPress: (e: KeyboardEvent<HTMLInputElement>) => void, setValue: Dispatch<SetStateAction<string>> }) {
+export default function SearchBar(props: {
+    handleSearch: () => void,
+    handleOnKeyPress: (e: KeyboardEvent<HTMLInputElement>) => void,
+    handleAddKeyword: (text: string) => void,
+    setValue: Dispatch<SetStateAction<string>>
+}) {
 
     const router = useRouter();
+    const { searchKeyword } = router.query;
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        console.log(e.target.value)
         props.setValue(e.target.value)
     };
 
@@ -25,10 +30,17 @@ export default function SearchBar(props: { handleSearch: () => void, handleOnKey
                 <input
                     className={style.searchinput}
                     placeholder='검색어를 입력해주세요.'
+                    defaultValue={searchKeyword}
                     onChange={handleChange}
                     onKeyDown={props.handleOnKeyPress}
                 />
-                <Image src={"/assets/images/icons/search.svg"} alt={"search"} width={20} height={20} onClick={props.handleSearch} priority />
+                <Image
+                    src={"/assets/images/icons/search.svg"}
+                    alt={"search"}
+                    width={20}
+                    height={20}
+                    onClick={props.handleSearch}
+                    priority />
             </div>
         </div>
     )
