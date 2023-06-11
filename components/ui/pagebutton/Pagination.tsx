9abfc0556cb31c0da7d1508ae4c9d6809b12174d
goeun <fuzzfuzz20@naver.com>
2023-06-11 23:45:11 +0900

    feat: 블럭 내역 페이지 컴포넌트 페이지 네이션 구현

---
+++ components/ui/pagebutton/Pagination.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import style from '@/components/ui/pagebutton/Pagination.module.css'
+
+interface Props {
+  total: number;
+  limit: number;
+  page: number;
+  setPage: React.Dispatch<React.SetStateAction<number>>;
+}
+
+export default function Pagination({ total, limit, page, setPage }: Props) {
+
+  const numPages = Math.ceil(total / limit);
+  console.log(numPages);
+
+  return (
+    <nav className={style.pagination}>
+      <button className={style.btn} onClick={() => setPage(1)} disabled={page === 1}>
+        &lt;
+      </button>
+      {
+        Array(limit)
+          .fill(total, 0, limit)
+          .map((_, i) => (
+            <button
+              key={i + 1}
+              onClick={() => setPage(i + 1)}
+              aria-current={page === i + 1 ? "page" : undefined}
+              className={page === i + 1 ? style.clickbtn : style.btn}
+            >
+              {i + 1}
+            </button>
+          ))}
+      <button className={style.btn} onClick={() => setPage(limit)} disabled={page === limit}>
+        &gt;
+      </button>
+    </nav>
+  )
+}
